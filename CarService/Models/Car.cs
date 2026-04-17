401778d156e0088b314621c175ac5e1f95c987e6
Stefan <103537403+Stefan-Kob@users.noreply.github.com>
2026-04-17 11:15:32 -0400

    Added Car model and installed efcore inmemory

---
+++ CarService/Models/Car.cs
@@ -0,0 +1,8 @@
+// Car class representing a car entity
+public class Car
+{
+    public int Id { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Horsepower { get; set; }
+}
